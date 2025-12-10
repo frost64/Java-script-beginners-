e59da3c105f301c82fddafdcdc4e35b10fb7b2da
Asjid Ahmed <31464246+frost64@users.noreply.github.com>
2025-12-10 03:40:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //Treats all JS Code as newer version
+
+// alert("Hi")
+// alert(5+5)  // we are using nodejs, not browser
+
+let name = "Asjid"       // string => ""
+let age = 27             // number
+let is_happy = false     // boolean => true/false
+let und;                 // undefined 
+let val = null           // null => standalone value
+// symbol => unique
+
+console.log(typeof und)  // can be used for datatype info for variables
+console.log(typeof 65)   // can be used for datatype info for direct inputs
+console.log(typeof undefined) // undefined is a datatype
+console.log(typeof null)      // while null is an object
