5c7533b8d0a01ff360cebfb13b64268c1b63b11b
Asjid Ahmed <31464246+frost64@users.noreply.github.com>
2025-12-10 21:13:04 +0000

    operations

--- 01_basics/03_conversionOps.js
+++ 01_basics/03_conversionOps.js
@@ -32,5 +32,47 @@ let tval1 = 1             // => String
 // let tval1 = null          // => String
 
 let convertStr = String(tval1)
-console.log(convertStr + "-STR")
-console.log(typeof convertStr)
+// console.log(convertStr + "-STR")
+// console.log(typeof convertStr)
+
+//------------------------------------ Operations -------------------------------------//
+
+let value = 3
+let negVal = -value
+// console.log(negVal);
+
+
+// console.log(2+2);        // Add
+// console.log(2-2);        // Subtract
+// console.log(2*2);        // Multiply
+// console.log(2**3);       // Power
+// console.log(2/3);        // Divide
+// console.log(2%3);        // Modulus
+
+let str1 = "Hi"
+let str2 = " "
+let str3 = "Asjid"
+
+let fullStr = str1+str2+str3
+// console.log(fullStr)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+let counter = 10
+// counter++;   // postfix increment
+// ++counter;   // prefix increment
+// console.log(counter)
+
+/*
+    prefix and postfix working and checks
+*/
+
+// let postCheck = counter++;
+// console.log(counter , postCheck)
+
+// let preCheck = ++counter;
+// console.log(counter , preCheck)
