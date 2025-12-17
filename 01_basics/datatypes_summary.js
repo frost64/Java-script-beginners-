39f7107266f50bd3dc9560c5ca6b99fdad9dfc98
Asjid Ahmed <31464246+frost64@users.noreply.github.com>
2025-12-17 21:54:21 +0000

    stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -35,3 +35,33 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//-------------------------------------------------------------------//
+
+// Stack (Primitive), Heap (Non-Primitive)
+let my_sign = "Frost"
+let copy_sign = my_sign;
+// console.log(my_sign)
+// console.log(copy_sign)
+
+copy_sign = "Frosty64"
+// console.log(my_sign)
+// console.log(copy_sign)
+
+
+let user1 = {
+    name : "Asjid",
+    Age: 27,
+    Location : "London"
+}
+
+let user2 = user1
+
+// console.log(user1.name)
+// console.log(user2.name)
+
+user2.name = my_sign
+
+console.log(user1.name)
+console.log(user2.name)     
